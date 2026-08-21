0bd227c5605824ba09f133ce2ac5e37f32c0b011
VikramTamang <158143586+VikramTamang@users.noreply.github.com>
2026-08-21 15:28:46 +0545

    test: load environment in static initializer for integration tests

--- src/test/java/com/fonepay/devportal/DevportalApplicationTests.java
+++ src/test/java/com/fonepay/devportal/DevportalApplicationTests.java
@@ -1,6 +1,5 @@
 package com.fonepay.devportal;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,8 +8,7 @@ import com.fonepay.devportal.common.config.EnvLoader;
 @SpringBootTest
 class DevportalApplicationTests {
 
-    @BeforeAll
-    static void setUp() {
+    static {
         EnvLoader.load();
     }
 
